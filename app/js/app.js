a6b48d8e3b17698335f835115822a9ce28245773
Mahesh Haldar <haldar.mahesh@gmail.com>
2015-02-20 20:10:56 +0530

    page navigation confirmation

--- app/js/app.js
+++ app/js/app.js
@@ -2,3 +2,6 @@
 //window.onload = app.rectangle.setupTools('canvas');
 var element = null;
 var currentCanvas = null
+window.onbeforeunload = function(){
+  return 'May be you dragged in wrong position or refreshed the page!';
+};
