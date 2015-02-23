8055c7af882af042cc0b02cf584e0d961a527bc7
Jaditi <Jaditi.1993@gmail.com>
2015-02-23 12:53:27 +0530

    Added url in images

--- app/js/save.js
+++ app/js/save.js
@@ -2,36 +2,41 @@ var saveFile = function(){
 
   var generateURL =function(src , title){
 
-    filepicker.setKey("AJyjszryUSGGEsDo5ntvqz");
-    console.log("src "+src ); 
-    var input = src.split(',')[1];
-    var fname = title;
-    var ftype = title.split('.')[1];
-    var mtype = 'image/'+ftype;
-    
-    
-    // console.log("File inp"+input);
+   filepicker.setKey("AJyjszryUSGGEsDo5ntvqz");
+   console.log("src "+src ); 
+   var input = src.split(',')[1];
+   var fname = title;
+   var ftype = title.split('.')[1];
+   var mtype = 'image/'+ftype;
+   
+   
+   // console.log("File inp"+input);
 
-    filepicker.store(
-      input,
-      {base64decode: true,
-       mimetype: mtype,
-       filename:  title},
-      function(Blob){
-        console.log("Store successful:" + JSON.stringify(Blob));
-        console.log(Blob.url);
-
-        return Blob.url;
-      },
-      function(FPError) {
-        console.log(FPError.toString());
-      },
-      function(progress) {
-        console.log("Loading: "+progress+"%");
-      }
-      );
-    enablePreview();
-  }
+   filepicker.store(
+     input,
+     {base64decode: true,
+      mimetype: mtype,
+      filename:  title},
+     function(Blob){
+       console.log("Store successful:" + JSON.stringify(Blob));
+       console.log(Blob.url);
+       var canvas = document.getElementById(fname +'c');
+       var setURL = document.createElement('p');
+       setURL.setAttribute('id', 'url');
+       var text = document.createTextNode("abcd");
+       setURL.appendChild(text);
+       canvas.appendChild(setURL);
+       
+     },
+     function(FPError) {
+       console.log(FPError.toString());
+     },
+     function(progress) {
+       console.log("Loading: "+progress+"%");
+     }
+     );
+   enablePreview();
+ }
 
   var generateJSON = function(){
     var list = document.getElementById('list');
