7497958a836fa775d806483c948738c7104cf65a
Jaditi <Jaditi.1993@gmail.com>
2015-02-24 16:53:17 +0530

    fontawesome

--- app/js/imageFile.js
+++ app/js/imageFile.js
@@ -1,41 +1,33 @@
+var imageFile = function() {
 
-var imageFile = function(){
+    var handleFileSelect = function(evt) {
 
-  
+        var files = evt.target.files; // FileList object
+        evt.stopPropagation();
+        evt.preventDefault();
+        var output = {
+            url: '',
+            img_name: ''
+        };
+        var files = evt.dataTransfer.files;
+        // Loop through the FileList and render image files as thumbnails.
+        if (files.length > 0) {
+            for (var i = 0, f; f = files[i]; i++) {
+                if (!f.type.match('image.*')) {
+                    continue;
+                }
 
-  var handleFileSelect = function(evt){
+                var reader = new FileReader();
+                reader.onload = (function(files) {
 
- var files = evt.target.files; // FileList object
-  evt.stopPropagation();
-  evt.preventDefault();
-  var output = {
-    url:'', img_name:''
-  };
-  var files = evt.dataTransfer.files;
-    // Loop through the FileList and render image files as thumbnails.
-    if(files.length > 0)
-    {
-        for (var i = 0, f; f = files[i]; i++) 
-        {
-            if (!f.type.match('image.*')) 
-               {
-                   continue;
-               }
-         
-          var reader = new FileReader();
-        reader.onload = (function(files) 
-        {
+                    return function(e) {
+                        output.url = e.target.result;
+                        output.img_name = escape(files.name.split('.')[0]);
+                        console.log(output.img_name);
+                        addImage(output);
+                    };
+                })(f);
 
-          return function(e) 
-          {
-            output.url = e.target.result;
-            output.img_name = escape(files.name.split('.')[0]);
-            console.log(output.img_name);
-            
-            addImage(output);
-            resizeImage(output);
-          };
-        })(f);
 
                 reader.readAsDataURL(f);
             }
@@ -78,35 +70,12 @@ var imageFile = function(){
     }
 
 
-}
+    return {
+        handleFileSelect: handleFileSelect,
+        handleDragOver: handleDragOver,
+        saveImage: saveImage,
 
+    }
 
-var resizeImage = function(object)
-{
-  maxW = 1000;
-  maxH = 1000;
-  var image = document.getElementById(object.img_name);
-  currW = parseInt(image.naturalWidth);
-  currH = parseInt(image.naturalHeight);
-  var ratio = currH / currW;
-  if(currW >= maxW && ratio <= 1){
-    currW = maxW;
-    currH = currW * ratio;
-  } 
-  else if(currH >= maxH){
-    currH = maxH;
-    currW = currH / ratio;
-  }
-  var canvas = document.getElementById(object.img_name+'c');
-  canvas.style.width = currW +"px";
-  canvas.style.height = currH +"px";
-}
-
-
-return {
-   handleFileSelect : handleFileSelect,
-   handleDragOver : handleDragOver,
-   saveImage : saveImage,
-   resizeImage : resizeImage
- }
 }();
+  
